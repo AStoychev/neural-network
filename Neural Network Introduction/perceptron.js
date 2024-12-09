ed4de3c5f21dcef48573f497fb9475902bb9806f
Stoychev <stoychev.nas@gmail.com>
2024-12-09 07:15:55 +0200

    Changes related with add perceptron class

--- Neural Network Introduction/perceptron.js
+++ Neural Network Introduction/perceptron.js
@@ -7,6 +7,29 @@ const trainInputs = [
 ];
 
 const trainLabels = [1, 1, 0, 0, 0];
-const weights = [0.1, 0.3];
-const b = 0.5; //bias
-const n = 0.1; //learning
+// const weights = [0.1, 0.3];
+// const b = 0.5; //bias
+// const n = 0.1; //learning
+
+
+class Perceptron {
+    constructor(learningRate = 0.1) {
+        this.weights = [0.1, -0.3];
+        this.bias = 0.5;
+        this.learningRate = learningRate
+    }
+
+    train(trainData) {
+        for (let i = 0; i < trainData.length; i++) {
+            let sum = this.bias;
+            let inputs = trainData[i];
+
+            for(let j = 0; j < inputs.length; j++) {
+                sum += inputs[j] * this.weights[j];
+            }
+        }
+    }
+}
+
+const perceptron = new Perceptron();
+perceptron.train(trainInputs);
