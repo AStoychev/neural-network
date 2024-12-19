7a647368e15cf45edb62ce9fcd08dd2b7fd6106d
BirchtechSolution <stoychev.nas@gmail.com>
2024-12-19 07:19:36 +0200

    Changes related with add offer and magin number in field

--- MnistReader.js
+++ MnistReader.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 function readIdxFile(filepath) {
     const data = fs.readFileSync(filepath);
+    let offes = 0;
+    const magicNumber = data.readUInt32BE(offes);
 }
 
 readIdxFile("./datasets/mnist/train-images.idx3-ubyte");
