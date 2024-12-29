3791ae07f7dd41607870681e7c70cc1d5ec28888
BirchtechSolution <stoychev.nas@gmail.com>
2024-12-29 15:17:20 +0200

    Changes related with

--- Neural Network Introduction/perceptron.js
+++ Neural Network Introduction/perceptron.js
@@ -41,6 +41,7 @@ class Perceptron {
 
     predict(inputs) {
         let sum = this.bias;
+
         for (let j = 0; j < inputs.length; j++) {
             sum += inputs[j] * this.weights[j];
         }
