64f3b10029f1aa742a074b5a02f24e348ed6ff62
Stoychev <stoychev.nas@gmail.com>
2025-01-13 07:12:02 +0200

    Changes related with add previewtestimage functionality in field

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
 import Navigation from "./pages/Navigations";
 import Home from "./pages/Home";
+import PreviewTestImages from "./pages/mnist/PreviewTestImage";
+
+function Router() {
+    const [pathname, setPathname] = useState(window.location.pathname);
+
+    switch (pathname) {
+        case "/mnist/test-images":
+            return <PreviewTestImages />;
+        default:
+            return <Home />
+    }
+}
 
 function App() {
 
@@ -8,6 +21,7 @@ function App() {
             <div className="app-container">
                 <Navigation />
                 <Home />
+                <PreviewTestImages />
             </div>
         </div>
     )
--- frontend/src/pages/Navigations.jsx
+++ frontend/src/pages/Navigations.jsx
@@ -4,25 +4,19 @@ function Navigation() {
         <nav>
             <ul>
                 <li>
-                    
                     <div className="link" onClick={() => alert("Go Home!")}>
                         Home
                     </div>
-
                 </li>
                 <li>
-
                     <div className="link" onClick={() => alert("Preview test images")}>
                         Preview test images
                     </div>
-
                 </li>
                 <li>
-
                     <div className="link" onClick={() => alert("Test Perceptron")}>
                         Test Perceptron
                     </div>
-                    
                 </li>
             </ul>
         </nav>
