301acde3475b98516161ec98628bc230b26216c3
Stoychev <stoychev.nas@gmail.com>
2025-01-15 07:14:02 +0200

    Changes related with add custom path for access pages view in field

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,41 @@
 import { useState } from "react";
-import Navigation from "./pages/Navigations";
+// import Navigation from "./pages/Navigations";
 import Home from "./pages/Home";
 import PreviewTestImages from "./pages/mnist/PreviewTestImage";
+
+function Navigation() {
+    const pushState = (path) => (e) => {
+        e.preventDefault();
+        window.history.pushState({}, "", path)
+    }
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <div className="link" onClick={pushState("/")}>
+                        Home
+                    </div>
+                </li>
+                <li>
+                    <div className="link" onClick={pushState("/mnist/test-images")}>
+                        Preview test images
+                    </div>
+                </li>
+                <li>
+                    <div className="link" onClick={pushState("/mnist/test-perceptron")}>
+                        Test Perceptron
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    )
+}
 
 function Router() {
 
     const [pathname, setPathname] = useState(window.location.pathname);
-    
+
     switch (pathname) {
         case "/mnist/test-images":
             return <PreviewTestImages />;
