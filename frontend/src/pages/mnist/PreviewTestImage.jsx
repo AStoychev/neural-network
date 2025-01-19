96f26a3e8c219e88125e685244d5e4988748c81c
AStoychev <stoychev.nas@gmail.com>
2025-01-19 21:18:54 +0200

    Changes related with functionality preview test image in field

--- frontend/src/pages/mnist/PreviewTestImage.jsx
+++ frontend/src/pages/mnist/PreviewTestImage.jsx
@@ -1,14 +1,14 @@
 function PreviewTestImages() {
     return (
-            <div className="page-container">
-                <div className="page-header">
-                    Mnist Test Images
-                </div>
-                <div className="page-content">
-                    <div className="image">
-                    </div>
+        <div className="page-container">
+            <div className="page-header">
+                Mnist Test Images
+            </div>
+            <div className="page-content">
+                <div className="image">
                 </div>
             </div>
+        </div>
     )
 }
 
