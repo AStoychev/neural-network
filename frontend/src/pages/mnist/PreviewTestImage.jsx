d1b354852fa3951f36bef5a4516d11333cdf7a81
AStoychev <stoychev.nas@gmail.com>
2025-01-21 07:15:28 +0200

    Changes related with create fetch test data functionality in field

--- frontend/src/pages/mnist/PreviewTestImage.jsx
+++ frontend/src/pages/mnist/PreviewTestImage.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 function PreviewTestImages() {
+
+    const [mnistData, setMnistData] = useState(null);
+
+    useEffect(() => {
+        fetch("/mnist/test-data.json")
+            .then(response => response.json())
+            .then(data => setMnistData(data));
+    }, []);
+
+    if (!mnistData) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
     return (
         <div className="page-container">
             <div className="page-header">
@@ -6,6 +25,7 @@ function PreviewTestImages() {
             </div>
             <div className="page-content">
                 <div className="image">
+                    {JSON.stringify(mnistData.labels)}
                 </div>
             </div>
         </div>
