9c4ac83a53f104534f444454d75e08576f36874d
AStoychev <stoychev.nas@gmail.com>
2025-02-15 21:41:34 +0200

    Changes related with add functionality in mnist reader in fields

--- MnistReader.js
+++ MnistReader.js
@@ -5,7 +5,7 @@ function readIdxFile(filepath) {
     let offset = 0;
     const magicNumber = data.readUInt32BE(offset);
     offset += 4;
-
+    
     const numberOfItems = data.readUint32BE(offset);
     offset += 4;
 
@@ -21,27 +21,22 @@ function readIdxFile(filepath) {
     } else {
         const rows = data.readUint32BE(offset);
         offset += 4;
-
         const cols = data.readUint32BE(offset);
         offset += 4;
-
         const images = [];
 
         for (let i = 0; i < numberOfItems; i++) {
             const image = [];
             for (let r = 0; r < rows; r++) {
                 const row = [];
-
                 for (let c = 0; c < cols; c++) {
                     row.push(data.readUint8(offset));
                     offset += 1;
                 };
                 image.push(row);
             };
-
             images.push(image);
         }
-        
         return { type: "images", data: images }
     };
 };
