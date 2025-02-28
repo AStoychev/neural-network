0304f34830e497aeacd340df8d3b5c7893b99410
AStoychev <stoychev.nas@gmail.com>
2025-02-28 07:17:56 +0200

    Changes related with add scan functionality for number in mnist reader in field

--- MnistReader.js
+++ MnistReader.js
@@ -7,7 +7,6 @@ function readIdxFile(filepath) {
     offset += 4;
     
     const numberOfItems = data.readUint32BE(offset);
-    
     offset += 4;
 
     if (magicNumber === 2049) {
@@ -35,9 +34,9 @@ function readIdxFile(filepath) {
                     row.push(data.readUint8(offset));
                     offset += 1;
                 };
-                
                 image.push(row);
             };
+
             images.push(image);
         }
         return { type: "images", data: images }
